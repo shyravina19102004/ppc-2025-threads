c1729a45f26347db350dffee8484b02a370cae2d
shyravina19102004 <shyravina19102004@gmail.com>
2025-04-29 19:20:34 +0300

    11

--- tasks/stl/shuravina_o_hoare_simple_merger_std/src/ops_stl.cpp
+++ tasks/stl/shuravina_o_hoare_simple_merger_std/src/ops_stl.cpp
@@ -7,34 +7,10 @@
 
 namespace shuravina_o_hoare_simple_merger_stl {
 
-namespace {
-
-void Merge(std::vector<int>& arr, int left, int mid, int right) {
-  std::vector<int> temp(right - left + 1);
-  int i = left;
-  int j = mid + 1;
-  int k = 0;
-
-  while (i <= mid && j <= right) {
-    temp[k++] = (arr[i] <= arr[j]) ? arr[i++] : arr[j++];
-  }
-  while (i <= mid) {
-    temp[k++] = arr[i++];
-  }
-  while (j <= right) {
-    temp[k++] = arr[j++];
-  }
-
-  std::ranges::copy(temp, arr.begin() + left);
-}
-
-}  // namespace
-
 bool TestTaskSTL::PreProcessingImpl() {
   if (task_data->inputs_count.empty() || task_data->outputs_count.empty()) {
     return false;
   }
-
   auto* in_ptr = reinterpret_cast<int*>(task_data->inputs[0]);
   input_ = std::vector<int>(in_ptr, in_ptr + task_data->inputs_count[0]);
   output_ = std::vector<int>(task_data->outputs_count[0], 0);
@@ -42,33 +18,20 @@ bool TestTaskSTL::PreProcessingImpl() {
 }
 
 bool TestTaskSTL::ValidationImpl() {
-  if (task_data->inputs_count.empty() || task_data->outputs_count.empty()) {
-    return false;
-  }
-  return task_data->inputs_count[0] == task_data->outputs_count[0];
+  return !task_data->inputs_count.empty() && !task_data->outputs_count.empty() &&
+         task_data->inputs_count[0] == task_data->outputs_count[0];
 }
 
 void TestTaskSTL::QuickSort(std::vector<int>& arr, int left, int right) {
-  if (left >= right) {
-    return;
-  }
+  if (left >= right) return;
 
   int pivot = arr[(left + right) / 2];
-  int i = left;
-  int j = right;
+  int i = left, j = right;
 
   while (i <= j) {
-    while (arr[i] < pivot) {
-      i++;
-    }
-    while (arr[j] > pivot) {
-      j--;
-    }
-    if (i <= j) {
-      std::swap(arr[i], arr[j]);
-      i++;
-      j--;
-    }
+    while (arr[i] < pivot) i++;
+    while (arr[j] > pivot) j--;
+    if (i <= j) std::swap(arr[i++], arr[j--]);
   }
 
   QuickSort(arr, left, j);
@@ -76,36 +39,35 @@ void TestTaskSTL::QuickSort(std::vector<int>& arr, int left, int right) {
 }
 
 void TestTaskSTL::ParallelQuickSort(std::vector<int>& arr, int left, int right) {
-  if (left >= right) {
-    return;
-  }
+  if (left >= right) return;
 
   int pivot = arr[(left + right) / 2];
-  int i = left;
-  int j = right;
+  int i = left, j = right;
 
   while (i <= j) {
-    while (arr[i] < pivot) {
-      i++;
-    }
-    while (arr[j] > pivot) {
-      j--;
-    }
-    if (i <= j) {
-      std::swap(arr[i], arr[j]);
-      i++;
-      j--;
-    }
+    while (arr[i] < pivot) i++;
+    while (arr[j] > pivot) j--;
+    if (i <= j) std::swap(arr[i++], arr[j--]);
   }
 
 #pragma omp parallel sections
   {
 #pragma omp section
     { ParallelQuickSort(arr, left, j); }
-
 #pragma omp section
     { ParallelQuickSort(arr, i, right); }
   }
+}
+
+void TestTaskSTL::Merge(std::vector<int>& arr, int left, int mid, int right) {
+  std::vector<int> temp(right - left + 1);
+  int i = left, j = mid + 1, k = 0;
+
+  while (i <= mid && j <= right) temp[k++] = (arr[i] <= arr[j]) ? arr[i++] : arr[j++];
+  while (i <= mid) temp[k++] = arr[i++];
+  while (j <= right) temp[k++] = arr[j++];
+
+  std::copy(temp.begin(), temp.end(), arr.begin() + left);
 }
 
 bool TestTaskSTL::RunImpl() {
@@ -114,20 +76,17 @@ bool TestTaskSTL::RunImpl() {
     return true;
   }
 
-  const auto size = static_cast<int>(input_.size());
+  const int size = input_.size();
   ParallelQuickSort(input_, 0, size - 1);
-  Merge(input_, 0, (size / 2) - 1, size - 1);
+  Merge(input_, 0, size / 2 - 1, size - 1);
   output_ = input_;
   return true;
 }
 
 bool TestTaskSTL::PostProcessingImpl() {
-  if (output_.empty()) {
-    return true;
-  }
-
+  if (output_.empty()) return true;
   auto* out_ptr = reinterpret_cast<int*>(task_data->outputs[0]);
-  std::ranges::copy(output_, out_ptr);
+  std::copy(output_.begin(), output_.end(), out_ptr);
   return true;
 }
 
