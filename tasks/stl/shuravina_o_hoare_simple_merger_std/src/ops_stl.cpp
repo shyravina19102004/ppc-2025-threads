6b26fcba71ad3a8c12324ccf2f11642436b0dc43
shyravina19102004 <shyravina19102004@gmail.com>
2025-05-12 20:50:40 +0300

    6

--- tasks/stl/shuravina_o_hoare_simple_merger_std/src/ops_stl.cpp
+++ tasks/stl/shuravina_o_hoare_simple_merger_std/src/ops_stl.cpp
@@ -1,7 +1,10 @@
 #include "stl/shuravina_o_hoare_simple_merger_std/include/ops_stl.hpp"
 
 #include <algorithm>
+#include <execution>
+#include <future>
 #include <memory>
+#include <thread>
 #include <utility>
 #include <vector>
 
@@ -36,45 +39,78 @@ void TestTaskSTL::QuickSort(std::vector<int>& arr, int left, int right) {
     return;
   }
 
-  int pivot = arr[(left + right) / 2];
+  int mid = left + (right - left) / 2;
+  if (arr[left] > arr[mid]) std::swap(arr[left], arr[mid]);
+  if (arr[left] > arr[right]) std::swap(arr[left], arr[right]);
+  if (arr[mid] > arr[right]) std::swap(arr[mid], arr[right]);
+  int pivot = arr[mid];
+  std::swap(arr[mid], arr[right - 1]);
+
   int i = left;
-  int j = right;
-
-  while (i <= j) {
-    while (arr[i] < pivot) {
-      i++;
+  int j = right - 1;
+  while (true) {
+    while (arr[++i] < pivot) {
     }
-    while (arr[j] > pivot) {
-      j--;
+    while (arr[--j] > pivot) {
     }
-    if (i <= j) {
-      std::swap(arr[i], arr[j]);
-      i++;
-      j--;
-    }
+    if (i >= j) break;
+    std::swap(arr[i], arr[j]);
   }
+  std::swap(arr[i], arr[right - 1]);
 
-  QuickSort(arr, left, j);
-  QuickSort(arr, i, right);
+  const int parallel_threshold = 5000;
+  if (right - left > parallel_threshold) {
+    auto future = std::async(std::launch::async, [&arr, left, i]() { QuickSort(arr, left, i - 1); });
+    QuickSort(arr, i + 1, right);
+    future.get();
+  } else {
+    QuickSort(arr, left, i - 1);
+    QuickSort(arr, i + 1, right);
+  }
 }
 
 void TestTaskSTL::MergeHelper(std::vector<int>& arr, int left, int mid, int right) {
   std::vector<int> temp(right - left + 1);
-  int i = left;
-  int j = mid + 1;
-  int k = 0;
 
-  while (i <= mid && j <= right) {
-    temp[k++] = (arr[i] <= arr[j]) ? arr[i++] : arr[j++];
-  }
-  while (i <= mid) {
-    temp[k++] = arr[i++];
-  }
-  while (j <= right) {
-    temp[k++] = arr[j++];
+  const int parallel_threshold = 10000;
+  if (right - left > parallel_threshold) {
+    auto future = std::async(std::launch::async, [&arr, &temp, left, mid, right]() {
+      int i = left;
+      int j = mid + 1;
+      int k = 0;
+      while (i <= mid && j <= mid + (right - left) / 2) {
+        temp[k++] = (arr[i] <= arr[j]) ? arr[i++] : arr[j++];
+      }
+      while (i <= mid) temp[k++] = arr[i++];
+    });
+
+    int i = mid + 1 + (right - left) / 2;
+    int j = mid + 1;
+    int k = (right - left) / 2 + 1;
+    while (i <= right && j <= right) {
+      temp[k++] = (arr[i] <= arr[j]) ? arr[i++] : arr[j++];
+    }
+    while (i <= right) temp[k++] = arr[i++];
+    while (j <= right) temp[k++] = arr[j++];
+
+    future.get();
+  } else {
+    int i = left;
+    int j = mid + 1;
+    int k = 0;
+    while (i <= mid && j <= right) {
+      temp[k++] = (arr[i] <= arr[j]) ? arr[i++] : arr[j++];
+    }
+    while (i <= mid) temp[k++] = arr[i++];
+    while (j <= right) temp[k++] = arr[j++];
   }
 
-  std::ranges::copy(temp, arr.begin() + left);
+  const int copy_threshold = 5000;
+  if (temp.size() > copy_threshold) {
+    std::copy(std::execution::par, temp.begin(), temp.end(), arr.begin() + left);
+  } else {
+    std::copy(temp.begin(), temp.end(), arr.begin() + left);
+  }
 }
 
 bool TestTaskSTL::RunImpl() {
@@ -86,6 +122,7 @@ bool TestTaskSTL::RunImpl() {
   const int size = static_cast<int>(input_.size());
   QuickSort(input_, 0, size - 1);
   MergeHelper(input_, 0, (size / 2) - 1, size - 1);
+
   output_ = input_;
   return true;
 }
@@ -95,7 +132,13 @@ bool TestTaskSTL::PostProcessingImpl() {
     return true;
   }
   int* out_ptr = reinterpret_cast<int*>(task_data->outputs[0]);
-  std::ranges::copy(output_, out_ptr);
+
+  if (output_.size() > 5000) {
+    std::copy(std::execution::par, output_.begin(), output_.end(), out_ptr);
+  } else {
+    std::copy(output_.begin(), output_.end(), out_ptr);
+  }
+
   return true;
 }
 
