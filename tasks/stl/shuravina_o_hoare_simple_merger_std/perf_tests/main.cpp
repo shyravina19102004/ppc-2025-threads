537f53ca93bb381b9c2b0f4144eb499f0d3910cb
shyravina19102004 <shyravina19102004@gmail.com>
2025-05-15 18:00:39 +0300

    17

--- tasks/stl/shuravina_o_hoare_simple_merger_std/perf_tests/main.cpp
+++ tasks/stl/shuravina_o_hoare_simple_merger_std/perf_tests/main.cpp
@@ -3,48 +3,47 @@
 #include <chrono>
 #include <cstdint>
 #include <memory>
+#include <random>
 #include <stl/shuravina_o_hoare_simple_merger_std/include/ops_stl.hpp>
-#include <thread>
 #include <vector>
 
 #include "core/perf/include/perf.hpp"
 #include "core/task/include/task.hpp"
 
 TEST(shuravina_o_hoare_simple_merger_stl, test_pipeline_run) {
-  constexpr int kCount = 50000;
-  std::vector<int> in(kCount, 0);
-  std::vector<int> out(kCount, 0);
+  constexpr int kCount = 512000;
+  std::vector<double> in(kCount, 0);
 
-  for (int i = 0; i < kCount; i++) {
-    in[i] = kCount - i;
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dist(-1000.0, 1000.0);
+  for (auto& val : in) {
+    val = dist(gen);
   }
 
-  {
-    auto task_data = std::make_shared<ppc::core::TaskData>();
-    task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
-    task_data->inputs_count.emplace_back(in.size());
-    task_data->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
-    task_data->outputs_count.emplace_back(out.size());
+  std::vector<double> out(kCount, 0);
 
-    auto test_task = std::make_shared<shuravina_o_hoare_simple_merger_stl::TestTaskSTL>(task_data);
+  auto task_data = std::make_shared<ppc::core::TaskData>();
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
+  task_data->inputs_count.emplace_back(in.size());
+  task_data->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
+  task_data->outputs_count.emplace_back(out.size());
 
-    auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
-    perf_attr->num_running = 10;
+  auto test_task = std::make_shared<shuravina_o_hoare_simple_merger_stl::TestTaskSTL>(task_data);
 
-    auto perf_results = std::make_shared<ppc::core::PerfResults>();
+  auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
+  perf_attr->num_running = 10;
+  perf_attr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point.time_since_epoch()).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
 
-    auto start = std::chrono::high_resolution_clock::now();
-    auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task);
-    perf_analyzer->PipelineRun(perf_attr, perf_results);
+  auto perf_results = std::make_shared<ppc::core::PerfResults>();
 
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration<double>(end - start).count();
-    if (duration < 0.05) {
-      std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>((0.05 - duration) * 1000)));
-    }
-
-    ppc::core::Perf::PrintPerfStatistic(perf_results);
-  }
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task);
+  perf_analyzer->PipelineRun(perf_attr, perf_results);
+  ppc::core::Perf::PrintPerfStatistic(perf_results);
 
   for (int i = 0; i < kCount - 1; i++) {
     ASSERT_LE(out[i], out[i + 1]);
@@ -52,40 +51,39 @@ TEST(shuravina_o_hoare_simple_merger_stl, test_pipeline_run) {
 }
 
 TEST(shuravina_o_hoare_simple_merger_stl, test_task_run) {
-  constexpr int kCount = 50000;
-  std::vector<int> in(kCount, 0);
-  std::vector<int> out(kCount, 0);
+  constexpr int kCount = 512000;
+  std::vector<double> in(kCount, 0);
 
-  for (int i = 0; i < kCount; i++) {
-    in[i] = kCount - i;
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dist(-1000.0, 1000.0);
+  for (auto& val : in) {
+    val = dist(gen);
   }
 
-  {
-    auto task_data = std::make_shared<ppc::core::TaskData>();
-    task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
-    task_data->inputs_count.emplace_back(in.size());
-    task_data->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
-    task_data->outputs_count.emplace_back(out.size());
+  std::vector<double> out(kCount, 0);
 
-    auto test_task = std::make_shared<shuravina_o_hoare_simple_merger_stl::TestTaskSTL>(task_data);
+  auto task_data = std::make_shared<ppc::core::TaskData>();
+  task_data->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
+  task_data->inputs_count.emplace_back(in.size());
+  task_data->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
+  task_data->outputs_count.emplace_back(out.size());
 
-    auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
-    perf_attr->num_running = 10;
+  auto test_task = std::make_shared<shuravina_o_hoare_simple_merger_stl::TestTaskSTL>(task_data);
 
-    auto perf_results = std::make_shared<ppc::core::PerfResults>();
+  auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
+  perf_attr->num_running = 10;
+  perf_attr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point.time_since_epoch()).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
 
-    auto start = std::chrono::high_resolution_clock::now();
-    auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task);
-    perf_analyzer->TaskRun(perf_attr, perf_results);
+  auto perf_results = std::make_shared<ppc::core::PerfResults>();
 
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration<double>(end - start).count();
-    if (duration < 0.05) {
-      std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>((0.05 - duration) * 1000)));
-    }
-
-    ppc::core::Perf::PrintPerfStatistic(perf_results);
-  }
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task);
+  perf_analyzer->TaskRun(perf_attr, perf_results);
+  ppc::core::Perf::PrintPerfStatistic(perf_results);
 
   for (int i = 0; i < kCount - 1; i++) {
     ASSERT_LE(out[i], out[i + 1]);
