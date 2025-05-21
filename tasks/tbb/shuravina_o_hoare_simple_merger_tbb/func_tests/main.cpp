c1b6f40806a87f1f7117cd90ed13b9b317b6b0ae
shyravina19102004 <shyravina19102004@gmail.com>
2025-05-21 18:16:05 +0300

    25

--- tasks/tbb/shuravina_o_hoare_simple_merger_tbb/func_tests/main.cpp
+++ tasks/tbb/shuravina_o_hoare_simple_merger_tbb/func_tests/main.cpp
@@ -62,25 +62,6 @@ TEST(shuravina_o_hoare_simple_merger_tbb, test_random_array) {
   ASSERT_TRUE(IsSorted(out));
 }
 
-TEST(shuravina_o_hoare_simple_merger_tbb, test_empty_array) {
-  std::vector<int> in = {};
-  std::vector<int> out(in.size(), 0);
-
-  auto task_data_tbb = std::make_shared<ppc::core::TaskData>();
-  task_data_tbb->inputs.emplace_back(reinterpret_cast<uint8_t*>(in.data()));
-  task_data_tbb->inputs_count.emplace_back(in.size());
-  task_data_tbb->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
-  task_data_tbb->outputs_count.emplace_back(out.size());
-
-  auto sorter = std::make_shared<shuravina_o_hoare_simple_merger_tbb::HoareSortTBB>(task_data_tbb);
-  ASSERT_EQ(sorter->Validation(), true);
-  sorter->PreProcessing();
-  sorter->Run();
-  sorter->PostProcessing();
-
-  EXPECT_EQ(out, in);
-}
-
 TEST(shuravina_o_hoare_simple_merger_tbb, test_sorted_array) {
   std::vector<int> in = {1, 2, 3, 4, 5, 6};
   std::vector<int> out(in.size(), 0);
